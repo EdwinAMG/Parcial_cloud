3dac3d197e0f09017c64d1da9faeba478fd7c9ac
Edwin M <andresedwinmg14@gmail.com>
2023-09-04 08:18:53 -0500

    Todo

---
+++ parcial/src/App.js
@@ -0,0 +1,20 @@
+import Tareas from "./componentes/Tareas";
+import "./App.css";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import { Amplify, Auth } from "aws-amplify";
+import aws_exports from "./aws-exports";
+Amplify.configure(aws_exports);
+
+export default function App() {
+  return (
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <Tareas />
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
+  );
+}
---
+++ parcial/src/componentes/Tareas.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import "./Tareas.css";
+
+function Tareas() {
+  const [tasks, setTasks] = useState([]);
+  const [taskText, setTaskText] = useState("");
+
+  const handleInputChange = (event) => {
+    setTaskText(event.target.value);
+  };
+
+  const addTask = () => {
+    if (taskText.trim() !== "") {
+      setTasks([...tasks, taskText]);
+      setTaskText("");
+    }
+  };
+
+  const removeTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
+
+  return (
+    <div className="container">
+      <h1>Lista de Tareas</h1>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <span>{task}</span>
+            <button onClick={() => removeTask(index)}>Eliminar</button>
+          </li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        className="task-input"
+        placeholder="Agregar nueva tarea"
+        value={taskText}
+        onChange={handleInputChange}
+      />
+      <button onClick={addTask}>Agregar</button>
+    </div>
+  );
+}
+
+export default Tareas;
